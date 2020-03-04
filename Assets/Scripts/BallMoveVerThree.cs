dae7d09704694601c4cbd5814c4ad1db6230ce4f
RedLegGames <60692924+DjKarp@users.noreply.github.com>
2020-03-04 13:38:47 +0700

    Initial commit

---
+++ Assets/Scripts/BallMoveVerThree.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoveVerThree : MonoBehaviour
+{
+
+    [Header("Скорость движения")]
+    [SerializeField]
+    private float speed = 1.0f;
+
+    private Transform m_Transform;
+    private Rigidbody2D m_Rigidbody2D;
+
+    private Vector3 mousePosition;
+
+    private bool isStartMove = false;
+
+    //для преобразования при повороте
+    private Vector3 xDirection;
+    private Vector3 yDirection;
+    private Vector3 zDirection;
+
+    private void Awake()
+    {
+
+        m_Transform = gameObject.transform;
+        m_Rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        
+        if (!isStartMove)
+        {
+
+            if (Input.GetMouseButtonUp(0))
+            {
+
+                isStartMove = true;
+
+                m_Rigidbody2D.AddForce((GetOurMouseMosition() - m_Transform.position) * speed * 10 * Time.deltaTime, ForceMode2D.Force);
+
+                LookAt2D(GetOurMouseMosition() - m_Transform.position);
+
+            }
+
+        }
+
+    }
+
+    private void LateUpdate()
+    {
+
+        if (isStartMove) LookAt2D(m_Rigidbody2D.velocity);
+
+    }
+
+    private Vector3 GetOurMouseMosition()
+    {
+
+        return mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+
+    }
+
+    public void LookAt2D(Vector3 lookTarget)
+    {
+
+        //Желаемое направление оси X, от которого устанавливаем ось Y. Z ось обращена к нам.
+        xDirection = (lookTarget - transform.position).normalized;
+        yDirection = Quaternion.Euler(0, 0, 90) * xDirection;
+        zDirection = Vector3.forward;
+
+        m_Transform.rotation = Quaternion.LookRotation(zDirection, yDirection);
+
+    }
+
+}
+
---
+++ Assets/Scripts/BallistaWeapon.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallistaWeapon : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform shootPoint;
+
+    [SerializeField]
+    private Transform sprite;
+
+    [SerializeField]
+    private Image shootPowerSlider;
+
+    [SerializeField]
+    private float maxShootStrange = 10.0f;
+    [SerializeField]
+    private float minShootStrange = 0.0f;
+
+    private float currentShootStrange;
+
+    [SerializeField]
+    private float timeShootLoad = 5.0f;
+    private float currentTimeShootLoad = 0.0f;
+
+    private Vector3 mousePosition;
+    private Vector3 startPosition;
+
+    private Rigidbody2D m_Rigidbody2D;
+
+    private Transform m_Transform;
+
+
+
+    protected virtual void Awake()
+    {
+
+        currentShootStrange = minShootStrange;
+
+        ChangePowerSlider();
+
+        m_Rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+
+        m_Transform = gameObject.transform;
+
+        startPosition = m_Transform.position;
+
+    }
+
+    protected virtual void Update()
+    {
+
+        //sprite.position = shootPoint.position;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            StartCoroutine(ChangeStrangeShootPower());
+
+        }
+
+    }
+
+    IEnumerator ChangeStrangeShootPower()
+    {
+
+        m_Rigidbody2D.isKinematic = true;
+     
+        while (!Input.GetMouseButtonUp(0))
+        {
+
+            //m_Transform.position = shootPoint.position;
+
+            if (currentTimeShootLoad < timeShootLoad) currentTimeShootLoad += Time.deltaTime;
+
+            currentShootStrange = minShootStrange + (((maxShootStrange - minShootStrange) * currentTimeShootLoad) / timeShootLoad);
+
+            ChangePowerSlider();
+
+            yield return null;
+
+        }
+
+        m_Rigidbody2D.isKinematic = false;
+
+        m_Rigidbody2D.AddForce((GetOurMouseMosition() - m_Transform.position) * currentShootStrange * Time.deltaTime, ForceMode2D.Impulse);
+
+        currentTimeShootLoad = 0.0f;
+        currentShootStrange = minShootStrange;
+        ChangePowerSlider();
+
+    }
+
+    protected virtual void ChangePowerSlider()
+    {
+
+        shootPowerSlider.fillAmount = (currentShootStrange - minShootStrange) / (maxShootStrange - minShootStrange);
+
+    }
+
+    private Vector3 GetOurMouseMosition()
+    {
+
+        return mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+}
